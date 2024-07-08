33a62a216081dd486d805746b895b60b1c3f8073
Hilton Perantunes <hperantunes@gmail.com>
2024-07-07 23:38:53 -0400

    Lesson 2.16

--- scripts/shaders_101.gd
+++ scripts/shaders_101.gd
@@ -1,5 +1,6 @@
 extends Node2D
 
+@onready var sprite: Sprite2D = $Sprite2D
 
 #func _process(delta):
 	#var time = Time.get_ticks_msec() / 1000.0
@@ -7,3 +8,9 @@ extends Node2D
 	#
 	#print("TIME: " + str(time))
 	#print("a: " + str(a))
+
+
+func _process(delta):
+	if Input.is_action_just_pressed("set_color"):
+		var new_color = Color(randf(), randf(), randf())
+		sprite.material.set_shader_parameter("my_color", new_color)
